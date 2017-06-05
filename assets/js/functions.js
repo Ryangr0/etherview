5d51cc8211ac0d0b203ce448b5924d909980ca46
Ryan Grippeling <ryan@fringe.nl>
2017-06-05 23:44:06 +0200

    Added datepicker

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -100,9 +100,9 @@ function alignDates(transactions)
 
             transaction_for_data.date                   = date;
             transaction_for_data.earned                 = earned;
-            transaction_for_data.worth_at_time          = accounting.formatMoney(worth_at_time, {symbol: "$"});
-            transaction_for_data.earnings               = accounting.formatMoney(earnings, {symbol: "$"});
-            transaction_for_data.earnings_if_sold_today = accounting.formatMoney(earnings_if_sold_today, {symbol: "$"});
+            transaction_for_data.worth_at_time          = worth_at_time;
+            transaction_for_data.earnings               = earnings;
+            transaction_for_data.earnings_if_sold_today = earnings_if_sold_today;
 
             total.earned                 += parseFloat(earned);
             total.earnings               += parseFloat(earnings);
@@ -115,10 +115,85 @@ function alignDates(transactions)
 
         total.earnings               = accounting.formatMoney(total.earnings, {symbol: "$"});
         total.earnings_if_sold_today = accounting.formatMoney(total.earnings_if_sold_today, {symbol: "$"});
+
+
+        if ($('#group-by').val() != 'none') {
+            data = groupTransactions(data);
+        }
+
+        console.log(data);
+
+        data = formatTransactions(data);
+
+        console.log(data);
+
         processTransactions(data, today, total);
     };
 
     xhr.send();
+}
+
+function groupTransactions(transactions)
+{
+    var group_by          = $('#group-by').val();
+    var group_by_singular = group_by.substring(0, group_by.length - 1);
+    var group_by_format   = 'DD-MM-YYYY';
+
+    switch (group_by) {
+        case 'days':
+            group_by_format = 'DD-MM-YYYY';
+            break;
+        case 'weeks':
+            group_by_format = 'WW';
+            break;
+        case 'months':
+            group_by_format = 'MMM YYYY';
+            break;
+        case 'years':
+            group_by_format = 'YYYY';
+            break;
+    }
+
+    var transactions_to_return = {};
+    $.each(transactions, function(i, transaction) {
+        var date             = moment(transaction.date, 'MM-DD-YYYY');
+        var grouped_date     = date.utc().startOf(group_by_singular);
+        var formatted_date   = grouped_date.format(group_by_format);
+        var key              = grouped_date.format('X');
+
+        if(transactions_to_return.hasOwnProperty(key)) {
+            transactions_to_return[key].date                    = formatted_date;
+            transactions_to_return[key].earned                  += transaction.earned;
+            transactions_to_return[key].earnings                += transaction.earnings;
+            transactions_to_return[key].earnings_if_sold_today  += transaction.earnings_if_sold_today;
+            transactions_to_return[key].worth_at_time           += transaction.worth_at_time;
+        } else {
+            transactions_to_return[key]                         = {};
+            transactions_to_return[key].date                    = formatted_date;
+            transactions_to_return[key].earned                  = transaction.earned;
+            transactions_to_return[key].earnings                = transaction.earnings;
+            transactions_to_return[key].earnings_if_sold_today  = transaction.earnings_if_sold_today;
+            transactions_to_return[key].worth_at_time           = transaction.worth_at_time;
+        }
+    });
+
+    // $.makeArray(transactions_to_return);
+    // var result = Object.keys(transactions_to_return).map(function(e) {
+    //     return transactions_to_return[e];
+    // });
+    //
+    // console.log(result);
+
+    return transactions_to_return;
+}
+
+function formatTransactions(transactions)
+{
+    $.each(transactions, function(i, transaction) {
+        transaction.worth_at_time          = accounting.formatMoney(transaction.worth_at_time, {symbol: "$"});
+        transaction.earnings               = accounting.formatMoney(transaction.earnings, {symbol: "$"});
+        transaction.earnings_if_sold_today = accounting.formatMoney(transaction.earnings_if_sold_today, {symbol: "$"});
+    });
 }
 
 function processTransactions(transactions, today, total)
