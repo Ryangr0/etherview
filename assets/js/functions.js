3520eeba04ceb0bd6147cf5fc6519695ac60930c
Ryan Grippeling <ryan@fringe.nl>
2017-06-06 22:28:41 +0200

    Datatable works, footer with total data doesn't work yet.

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -128,7 +128,39 @@ function processTransactions(transactions, today, total)
     var rendered = Mustache.render(template, {'transactions' : transactions, 'today': today, 'total': total});
     $('#tax-table').html(rendered);
     $('#tax-table>table').DataTable({
-        paging: false
+        paging: false,
+        "footerCallback": function ( row, data, start, end, display ) {
+            var api = this.api(), data;
+
+            // Remove the formatting to get integer data for summation
+            var intVal = function ( i ) {
+                return typeof i === 'string' ?
+                i.replace(/[\$,]/g, '')*1 :
+                    typeof i === 'number' ?
+                        i : 0;
+            };
+
+            // Total over all pages
+            total = api
+                .column( 4 )
+                .data()
+                .reduce( function (a, b) {
+                    return intVal(a) + intVal(b);
+                }, 0 );
+
+            // Total over this page
+            pageTotal = api
+                .column( 4, { page: 'current'} )
+                .data()
+                .reduce( function (a, b) {
+                    return intVal(a) + intVal(b);
+                }, 0 );
+
+            // Update footer
+            $( api.column( 4 ).footer() ).html(
+                '$'+pageTotal +' ( $'+ total +' total)'
+            );
+        }
     });
     spinner.stop();
 }
